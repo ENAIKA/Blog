a1e3bc7967d5fb092adea79e6fbda19737fadc14
ENAIKA <naikaesther5@gmail.com>
2020-05-14 05:25:15 +0300

    modify routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,5 @@
 from . import auth
 from .. import db
-from sqlalchemy.dialects import postgresql#trying //ON CONFLICT (y) DO NOTHING
 from flask import render_template,redirect,url_for, flash,request
 from ..models import User
 from .forms import LoginForm,RegistrationForm
@@ -13,15 +12,13 @@ from ..email import mail_message
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        # import pdb; pdb.set_trace()
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        # import pdb; pdb.set_trace()
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
     
 @auth.route('/logout')
